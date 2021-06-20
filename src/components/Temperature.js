3c848987f0b0a2d8efb4de46bb34276a2a7f0d82
AlondraGascaL <73613690+AlondraGascaL@users.noreply.github.com>
2021-06-20 00:47:21 -0500

    temperatura

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,7 +1,12 @@
 import { Typography } from "@material-ui/core";
+// Importar modulos y estilos
+import { CircularProgressbar, buildStyles } from "sensores";
 import { useData } from "../context/DataContext";
 import Base from "./Base";
 import DataChart from "./DataChart";
+
+// Animacion
+import ChangingProgressProvider from "./ChangingProgressProvider";
 
 const Temperature = () => {
   const { temperature, temperatureHistory } = useData();
@@ -11,8 +16,24 @@ const Temperature = () => {
         <Typography>dentro de temperature</Typography>
       </Base>
       <DataChart data={temperatureHistory} from={1000} />
+      <ChangingProgressProvider values={temperature}>
+        {(value) => (
+          <CircularProgressbar
+            value={value}
+            text={`${value}°C`}
+            circleRatio={0.75}
+            styles={buildStyles({
+              rotation: 1 / 2 + 1 / 8,
+              strokeLinecap: "butt",
+              trailColor: "linear-gradient(to right bottom, #430089, #82ffa1)"
+            })}
+          />
+        )}
+      </ChangingProgressProvider>
     </>
   );
+
 };
+
 
 export default Temperature;
